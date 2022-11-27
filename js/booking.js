2932c2823a8a6a2eecf2bbdacb7bffe10c1a09f5
adrkaar <112952665+adrkaar@users.noreply.github.com>
2022-11-27 17:28:47 +0100

    Litt mer content

--- js/booking.js
+++ js/booking.js
@@ -3,7 +3,7 @@ fetch("../components/booking.component.html")
 .then(text => {
     let element = document.querySelector("script#booking");
     let newelem = document.createElement("div");
-    newelem.setAttribute("id", "booking-container");
+    newelem.setAttribute("id", "booking");
     newelem.innerHTML = text;
     element.parentNode.replaceChild(newelem, element);
 })
